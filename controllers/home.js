81b8969eeec5f7d2f234ef163aa4312c4797f69d
Omri Tintpulver <omri.tintpulver@gmail.com>
2016-10-22 22:49:24 -0400

    debugging of user after auth

--- controllers/home.js
+++ controllers/home.js
@@ -6,4 +6,10 @@ exports.index = (req, res) => {
   res.render('home', {
     title: 'Home'
   });
+
+  // dump the user for debugging
+  if (req.isAuthenticated()) {
+    console.log("authenticated as user:");
+    console.log(JSON.stringify(req.user, null, 4));
+  }
 };
